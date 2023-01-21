4ac9812b10ecb7536d52e94786159147823482e7
zach_simop <zachsimop@gmail.com>
2023-01-21 08:48:34 -0600

    window rendering

---
+++ AcornEngine/AcornEngine.cpp
@@ -0,0 +1,73 @@
+﻿// AcornEngine.cpp : Defines the entry point for the application.
+//
+
+#include "AcornEngine.h"
+#include "glad.h"
+#include "GLFW/glfw3.h"
+
+
+using namespace std;
+
+int main()
+{
+	glfwInit();
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Your God dam window", NULL, NULL);
+
+	if (window == NULL)
+	{
+		cout << "Failed to create GLFW window" << endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
+	{
+		std::cout << "Failed to initialize OpenGL context" << std::endl;
+		return 0;
+	}
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	static const GLfloat g_vertex_buffer_data[] = {
+   -1.0f, -1.0f, 0.0f,
+   1.0f, -1.0f, 0.0f,
+   0.0f,  1.0f, 0.0f,
+	};
+
+	// This will identify our vertex buffer
+	GLuint vertexbuffer;
+	// Generate 1 buffer, put the resulting identifier in vertexbuffer
+	glGenBuffers(1, &vertexbuffer);
+	// The following commands will talk about our 'vertexbuffer' buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	// Give our vertices to OpenGL.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	printf("OpenGL %d.%d\n", GLVersion.major, GLVersion.minor);
+
+	while (1) {
+		// 1st attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,
+			3,
+			GL_FLOAT,
+			GL_FALSE,
+			0,
+			(void*)0
+		);
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+		glDisableVertexAttribArray(0);
+	}
+
+	cout << "you've done it god dammit" << endl;
+
+	return 0;
+}
---
+++ out/build/x64-debug/lib/GLEW/examples/CMakeFiles/splitview.dir/glfw.i
@@ -0,0 +1,4 @@
+#line 1 "C:\\Users\\zachs\\vandy-c\\swp\\AcornEngine\\lib\\GLFW\\examples\\glfw.rc"
+
+GLFW_ICON               ICON            "glfw.ico"
+
