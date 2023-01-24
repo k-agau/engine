d21a223e16b007becbdb0bf2be18305a42ee1f46
zach_simop <zachsimop@gmail.com>
2023-01-24 16:30:41 -0600

    shader class update

--- AcornEngine/AcornEngine.cpp
+++ AcornEngine/AcornEngine.cpp
@@ -4,15 +4,28 @@
 #include "AcornEngine.h"
 #include "glad.h"
 #include "GLFW/glfw3.h"
+#include "Shader.h"
 
 
 using namespace std;
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window, GLfloat(&Object)[]);
+
 int main()
 {
-	glfwInit();
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Your God dam window", NULL, NULL);
+/*GLFW INIT AND CONFIG*/
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	#ifdef __APPLE__
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	#endif
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "AcornEngine.exe", NULL, NULL);
 
 	if (window == NULL)
 	{
@@ -22,6 +35,7 @@ int main()
 	}
 
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
 	{
@@ -29,45 +43,78 @@ int main()
 		return 0;
 	}
 
-	GLuint VertexArrayID;
-	glGenVertexArrays(1, &VertexArrayID);
-	glBindVertexArray(VertexArrayID);
-
-	static const GLfloat g_vertex_buffer_data[] = {
-   -1.0f, -1.0f, 0.0f,
-   1.0f, -1.0f, 0.0f,
-   0.0f,  1.0f, 0.0f,
-	};
-
-	// This will identify our vertex buffer
-	GLuint vertexbuffer;
-	// Generate 1 buffer, put the resulting identifier in vertexbuffer
-	glGenBuffers(1, &vertexbuffer);
-	// The following commands will talk about our 'vertexbuffer' buffer
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	// Give our vertices to OpenGL.
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
 	printf("OpenGL %d.%d\n", GLVersion.major, GLVersion.minor);
 
-	while (1) {
-		// 1st attribute buffer : vertices
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-		glVertexAttribPointer(
-			0,
-			3,
-			GL_FLOAT,
-			GL_FALSE,
-			0,
-			(void*)0
-		);
+	float triangle[] = {
+		// positions         // colors
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+	};
+
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	Shader myShader;
+	
+	while (!glfwWindowShouldClose(window))
+	{
+
+		processInput(window, triangle);
+
+		glClearColor(0.0f, 1.0f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		myShader.use();
+		//glBindVertexArray(VAO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
 		// Draw the triangle !
 		glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
-		glDisableVertexAttribArray(0);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
 
-	cout << "you've done it god dammit" << endl;
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	myShader.~Shader();
 
+	glfwTerminate();
 	return 0;
+}
+
+void processInput(GLFWwindow* window, GLfloat (&Object)[])
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+		int8_t i = 0;
+		while (i < 9) Object[i++] += 0.001;
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+		int8_t i = 0;
+		while (i < 9) Object[i++] -= 0.001;
+	}
+		
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
 }
---
+++ AcornEngine/Shader.h
@@ -0,0 +1,35 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include "glad.h" // include glad to get all the required OpenGL headers
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+
+class Shader
+{
+public:
+    // the program ID
+    unsigned int ID;
+
+    //uses default paths
+    Shader();
+    // constructor reads with custom paths
+    Shader(const char* vertexPath, const char* fragmentPath);
+    // use/activate the shader
+    ~Shader();
+    void use();
+    // utility uniform functions
+    void setBool(const std::string& name, bool value) const;
+    void setInt(const std::string& name, int value) const;
+    void setFloat(const std::string& name, float value) const;
+
+private:
+    const char* ACORN_VERTEX_SHADER_PATH = "C:\\Users\\zachs\\vandy-c\\swp\\AcornEngine\\AcornEngine\\vertex_shader.vert";
+    const char* ACORN_FRAGMENT_SHADER_PATH = "C:\\Users\\zachs\\vandy-c\\swp\\AcornEngine\\AcornEngine\\fragment_shader.frag";
+};
+
+#endif
